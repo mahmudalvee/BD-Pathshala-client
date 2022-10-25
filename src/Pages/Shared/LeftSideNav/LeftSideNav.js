0ff4f0f26f164900aa71a74c31e5cc5694d27160
mahmudalvee <mahmud2.alvee@gmail.com>
2022-10-26 00:13:20 +0600

    fetching categories

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Link } from "react-router-dom";
+import './LeftSideNav.css';
 
 const LeftSideNav = () => {
-    return (
-        <div>
-            <h3>Left Side</h3>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/course-categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Left Side: {categories.length}</h3>
+      <div className="App">
+        <ListGroup>
+            {
+                categories.map(category => <Link className="link" to={`/course-category/${category.id}`}
+                    key={category.id}>
+                        <ListGroup.Item className="txt-link" variant="info">{category.name}</ListGroup.Item>
+                    </Link> )
+            }
+        </ListGroup>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSideNav;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Course from "../../Pages/Course/Course/Course";
+import CourseCategory from "../../Pages/CourseCategory/CourseCategory/CourseCategory";
 import Home from "../../Pages/Home/Home";
 
 export const routes= createBrowserRouter([
@@ -10,6 +12,14 @@ export const routes= createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/course-category/:id',
+                element: <CourseCategory></CourseCategory>
+            },
+            {
+                path: '/course/:id',
+                element: <Course></Course>
             }
         ]
     }
