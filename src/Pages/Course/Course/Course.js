02f8c3090fcde83e166705d08cc5ef68b6499e88
mahmudalvee <mahmud2.alvee@gmail.com>
2022-10-27 18:54:00 +0600

    details course

--- src/Pages/Course/Course/Course.js
+++ src/Pages/Course/Course/Course.js
@@ -2,15 +2,54 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 import './Course.css';
 
 const Course = () => {
     const course= useLoaderData();
-    const {_id, title, img, instructor, category_id, category_name, description, fee, total_enroll} = course
+    console.log(course);
+    const {_id, title, img, instructor, category_id, tags, email, category_name, description, fee, total_enroll} = course
     return (
         <div>
-            <h2>{course.title}</h2>
-            <Link className="ms-auto mb-1 me-1 my-1" to={`/enroll-course/${_id}`}><Button className=" fs-4 shadow rounded btn-width txt" variant="warning"><i className="bi bi-gift-fill txt"></i> Get Premium Access</Button></Link>
+            <div className="card-header text-end">
+                <Button className=" fs-4 shadow rounded btn-width txt trans" variant="warning"><i className="bi bi-file-earmark-arrow-down-fill txt"></i> Get Details</Button>
+            </div>
+            <Card>
+            <Card.Img variant="top" className='img-fluid rounded mx-auto d-block' src={img} />
+            <Card.Body>
+                <h2 className='txt text-center fw-bold'>{title}</h2>
+                <Card.Text className='text-dark'>
+                    {description}
+                </Card.Text>
+                <div className='txt'>
+                    <ul>
+                        {
+                            tags.map( tag => 
+                                <li
+                                key={tag}
+                                >{tag}</li>
+                            )
+                        }
+                    </ul>
+                </div>
+                <h3 className='txt'>Instructor: {instructor}</h3>
+                <h5 className='text-dark'><i className="bi bi-envelope-check"></i>: {email}</h5>
+                <h4 className='txt'><i className="bi bi-people-fill"></i> : {total_enroll} students</h4>
+                <Card.Footer className="d-flex justify-content-between">
+                    <div className='my-auto'>
+                        <h1><span className="badge rounded-pill text-bg-primary">Course fee: {fee}</span></h1>
+                    </div>
+                    <div>
+                    <Link to={`/enroll-course/${_id}`}><Button className=" fs-3 shadow-lg rounded btn-width txt trans" variant="warning"><i className="bi bi-gift-fill txt"></i> Get Premium Access</Button></Link>
+                    </div>
+                </Card.Footer>
+                <div className='text-center'>
+                <Link to={`/course-category/${category_id}`}>
+                    <button type="button" className="btn btn-outline-dark  shadow-sm rounded fs-5">Similar Courses</button>
+                </Link>
+                </div>
+            </Card.Body>
+        </Card>
         </div>
     );
 };
--- src/Pages/EnrollCourse/EnrollCourse/EnrollCourse.js
+++ src/Pages/EnrollCourse/EnrollCourse/EnrollCourse.js
@@ -1,14 +1,63 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 import './EnrollCourse.css';
 
 const EnrollCourse = () => {
     const course= useLoaderData();
     const {_id, title, img, instructor, category_id, category_name, description, fee, total_enroll} = course;
     return (
-        <div>
-            <h2>Premium Acces of: {title}</h2>
-            <h4>instructor: {instructor}</h4>
+        <div className='txt mt-3 bg-light rounded p-5'>   
+            <h1 className='text-center'><span className="badge rounded-pill text-bg-warning">Get Premium Access of '{title}'</span></h1>
+             <Form className='mt-2'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Course Name</Form.Label>
+                    <Form.Control readOnly defaultValue={title} type="text"/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Course Category</Form.Label>
+                    <Form.Control readOnly defaultValue={category_name} type="text"/>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" >
+                    <Form.Label>Mobile number</Form.Label>
+                    <Form.Control type="text" placeholder="number" />
+                </Form.Group>
+
+                <div className='text-center text-dark'>
+                    <h2>Total fee: {fee}</h2>
+                    <h4>Payment number: 01XXXXXXXXX</h4>
+                    <h4>Payment acceptable: Bkash/ Nogod/ Rocket.</h4>
+                </div>
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Bkash" />
+                    <Form.Check type="checkbox" label="Nogod" />
+                    <Form.Check type="checkbox" label="Rocket" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" >
+                    <Form.Label>Transaction ID</Form.Label>
+                    <Form.Control type="text" placeholder="Transaction ID" />
+                </Form.Group>
+
+                <Button type="submit" className="fs-4 shadow rounded btn-width txt trans" variant="warning"><i className="bi bi-gift-fill txt"></i> Payment Complete</Button>
+            </Form>   
         </div>
     );
 };
